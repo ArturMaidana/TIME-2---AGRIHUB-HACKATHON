5d9285fada1f2de0afdb38515e4c8fa07729f76d
Artur Maidana <132027060+ArturMaidana@users.noreply.github.com>
2026-09-20 14:02:20 -0400

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,3 +1,9 @@
-import { app } from '../server/app.js';
+import { handleApi } from '../server/routes/api-routes.js';
 
-export default app;
+export default async function handler(request, response) {
+  const protocol = request.headers['x-forwarded-proto'] || 'https';
+  const host = request.headers['x-forwarded-host'] || request.headers.host || 'localhost';
+  const url = new URL(request.url, `${protocol}://${host}`);
+  
+  return handleApi(request, response, url);
+}
